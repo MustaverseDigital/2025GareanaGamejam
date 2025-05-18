d9ea96678784f47399bd810e7ebebc1cf4e18a5d
Wallace Hsieh <wallace.hsieh@aktsk.com>
2025-05-18 12:35:51 +0800

    Update countdown and score calculation

--- Assets/Scripts/BetManViewManager.cs
+++ Assets/Scripts/BetManViewManager.cs
@@ -1,57 +1,57 @@
-using System;
-using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 using DEAL.Event;
-using UnityEngine.Serialization;
-using System.Collections;
 using UnityEngine.UI;
 
 namespace DEAL.UI
 {
-    public class BetManViewManager : MonoBehaviour, IEventListener
+    public class BetManViewManager : ViewManager
     {
-        static BetManViewManager _instance = null;
-
-        public static BetManViewManager Instance
-        {
-            get
-            {
-                if (_instance == null)
-                {
-                    _instance = FindObjectOfType<BetManViewManager>();
-                    if (_instance == null)
-                    {
-                        var _cache = new GameObject("BetManViewManager");
-                        _instance = _cache.AddComponent<BetManViewManager>();
-                    }
-                }
-
-                return _instance;
-            }
-        }
-
-        protected delegate void ViewEventHandler(ViewEventPayload payload);
-        private readonly Dictionary<string, ViewEventHandler> _viewEventHandlers = new Dictionary<string, ViewEventHandler>();
-
-        [FormerlySerializedAs("m_cardPlayerUI")]
-        [SerializeField]
-        protected DialogManager mDialogManager;
-
         [SerializeField]
         private Animator mAnimator;
 
         [SerializeField]
         private Slider mCountDownSlider;
+        
+        private bool isCountdownActive = false;
 
-        private void Awake()
+        protected override void Awake()
         {
-            AttachViewEventListeners();
-            NetworkEventSystem.AttachListener(EventType.VIEW_EVENT, this);
+            _instance = this;
+            base.Awake();
         }
 
-        private void OnDestroy()
+        protected override void OnStopCountDown(ViewEventPayload payload)
         {
-            NetworkEventSystem.DetachListener(EventType.VIEW_EVENT, this);
+            isCountdownActive = false;
+        }
+
+        protected override IEnumerator CountdownCoroutine(float maxTime, float duration, string callbackEventKey)
+        {
+            isCountdownActive = true;
+            mCountDownSlider.maxValue = maxTime == 0 ? duration : maxTime;
+            mCountDownSlider.value = duration;
+            while (duration > 0 && isCountdownActive)
+            {
+                duration -= Time.deltaTime;
+                mCountDownSlider.value = duration;
+                yield return null;
+            }
+
+            if (isCountdownActive)
+            {
+                NetworkEventSystem.BroadcastEvent(EventType.STATE_EVENT, new ViewEventPayload { eventKey = callbackEventKey });
+                isCountdownActive = false;
+            }
+            NetworkEventSystem.BroadcastEvent(EventType.ACTION_EVENT, new ActionPayload
+            {
+                eventKey = "PropertyChange",
+                receiverViewIds = DealEngine.Instance.AllPlayerIds,
+                senderViewId = DealEngine.Instance.LocalAvatarId,
+                dataType = GlobalEnum.PropertyType.Float,
+                data = duration,
+                actionName = "AvailableTime",
+            });
         }
 
         public void OnClickDealJokers()
@@ -61,86 +61,6 @@ namespace DEAL.UI
                 eventKey = "FinishSelect"
             };
             NetworkEventSystem.BroadcastEvent(EventType.STATE_EVENT, payload);
-        }
-
-        public void OnEvent(EventMsg e)
-        {
-            switch (e.Type)
-            {
-                case EventType.VIEW_EVENT:
-                    {
-                        if (e.Data is ViewEventPayload payload)
-                        {
-                            if (_viewEventHandlers.ContainsKey(payload.eventKey))
-                            {
-                                _viewEventHandlers[payload.eventKey](payload);
-                            }
-
-                            Debug.Log("payload.eventKey: " + payload.eventKey);
-
-                            if (payload.data is CountdownData countdownData)
-                            {
-                                Debug.Log("payload.data: " + payload.data);
-                                StartCoroutine(CountdownCoroutine(countdownData.duration, countdownData.callbackEventKey));
-                            }
-                        }
-                        else
-                        {
-                            throw new System.Exception("Invalid Payload Type");
-                        }
-                        break;
-                    }
-                default:
-                    {
-                        throw new Exception("Invalid Event Type");
-                    }
-            }
-        }
-
-        protected void AttachViewEventListener(string eventKey, ViewEventHandler callback)
-        {
-            _viewEventHandlers[eventKey] = callback;
-        }
-
-        protected virtual void AttachViewEventListeners()
-        {
-            AttachViewEventListener("OpenDialog", OnOpenDialog);
-        }
-
-        private void OnOpenDialog(ViewEventPayload payload)
-        {
-            var data = (PopDialogData)payload.data;
-            if (data.TargetIds == null || data.TargetIds.Length == 0)
-            {
-                var localAvatar = DealEngine.Instance.LocalAvatar;
-                data.CallbackActionTargetId = localAvatar.PlayerId;
-                mDialogManager.OpenDialog(localAvatar, data);
-            }
-            else
-            {
-                foreach (var avatarId in data.TargetIds)
-                {
-                    data.CallbackActionTargetId = avatarId;
-                    var avatar = NetworkEventSystem.Find<Avatar>(avatarId);
-                    switch (avatar.AvatarType)
-                    {
-                        case GlobalEnum.AvatarType.Npc:
-                            avatar.OnNpcProcessDialog(data);
-                            break;
-                        case GlobalEnum.AvatarType.Player:
-                            mDialogManager.OpenDialog(avatar, data);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }
-        }
-
-        private IEnumerator CountdownCoroutine(float duration, string callbackEventKey)
-        {
-            yield return new WaitForSeconds(duration);
-            NetworkEventSystem.BroadcastEvent(EventType.STATE_EVENT, new ViewEventPayload { eventKey = callbackEventKey });
         }
 
         // Animator State Transition Methods
